d3b8d2883ed0e102208c73083810f810aeab7cca
wawayuqin <wawayuqin@163.com>
2019-01-07 15:41:42 +0800

    Add files via upload

---
+++ udp-sendch20.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import socket
+from getch20 import get_ch20
+import time
+ip_port = ('47.105.88.192',8888)
+sk = socket.socket()
+sk.connect(ip_port)
+sk.settimeout(5)
+
+while True:
+    data='20' #get_ch20()
+    sk.sendall(data)
+    time.sleep(5)
+
+
+sk.close()
+
